c16250da28b205865b70c0a3edb0792dee847394
sundayayandele <deaconsunday@gmail.com>
2021-05-07 12:41:29 +0200

    Third commit

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,2 +1,4 @@
-print ("Hello World")
-Welcome to my world of git practices
+print("Hello World")
+print("I welcome you to my world of git practices") 
+print("Welcome to my world of git practices")
+print("You are welcome to my world of git practices")
